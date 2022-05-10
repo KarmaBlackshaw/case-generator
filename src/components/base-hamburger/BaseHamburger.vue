420e2f3f4c5ab9d663d33a9e10a12c8e2a1de268
Karma Blackshaw <irenesejah29@gmail.com>
2022-05-10 13:48:15 +0800

    add base hamburger

---
+++ src/components/base-hamburger/BaseHamburger.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="base-hamburger hamburger">
+    <div class="hamburger__line hamburger__line--1"></div>
+    <div class="hamburger__line hamburger__line--2"></div>
+    <div class="hamburger__line hamburger__line--3"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseHamburger"
+}
+</script>
+
+<style lang="scss" scoped>
+@import './assets/scss/BaseHamburger';
+</style>
