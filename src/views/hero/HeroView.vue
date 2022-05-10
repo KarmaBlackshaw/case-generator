c0d91f8954d776c21aea64212ede71a1a83cf271
Karma Blackshaw <irenesejah29@gmail.com>
2022-05-10 17:18:02 +0800

    update hero styles

---
+++ src/views/hero/HeroView.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="hero">
+    <div class="hero__tagline">
+      A Simple <span class="accent">Case Converter</span>
+    </div>
+    <div class="hero__description">
+      An open source case converter for everyday use
+    </div>
+    <div class="hero__actions">
+      <a
+        class="btn btn--github"
+        target="blank"
+      >
+        <img
+          src="./assets/images/github-dark.png"
+          alt=""
+        >
+        <div>
+          <p class="title">This project is open source</p>
+          <p class="subtitle">Star and fork it on github</p>
+        </div>
+      </a>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'HeroView'
+}
+</script>
+
+<style lang="scss" scoped>
+@import './assets/scss/HeroView';
+</style>
+h e l l o  w o r l d
