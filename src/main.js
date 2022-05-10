ce937544b6ec1b2b7cd48dc7b195dfaad5c312e6
Karma Blackshaw <irenesejah29@gmail.com>
2022-05-10 18:37:51 +0800

    add vueuse head

--- src/main.js
+++ src/main.js
@@ -9,8 +9,12 @@ import '@/assets/scss/app/_@index.scss'
 // plugins
 import baseComponents from '@/plugins/baseComponents'
 
+// composables
+import { createHead } from '@vueuse/head'
+
 createApp(App)
   .use(baseComponents)
   .use(store)
   .use(router)
+  .use(createHead())
   .mount('#app')
