424c0f971c0fb4a5f396216da34d50778b23ec36
Ernie Jeash Villahermosa <40014179+KarmaBlackshaw@users.noreply.github.com>
2022-11-05 22:32:43 +0800

    wip: remove unused files

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,35 @@
+<script setup>
+</script>
+
+<template>
+  <section
+    class="
+      h-[44px]
+      flex
+      justify-center
+      items-center
+      text-gray-900/50 dark:text-gray-400
+      bg-gray-200 dark:bg-gray-800
+      border-t border-gray-200 dark:border-0
+      gap-[7px]
+      text-xs
+      font-mono
+    "
+  >
+    Made with
+    <a
+      href="https://vuejs.org/"
+      target="_blank"
+    >
+      <icon-ri:vuejs-fill class="text-sm" />
+    </a>
+    by
+    <a
+      href="https://jeash.tech"
+      class="font-bold"
+      target="_blank"
+    >
+      @Jeash
+    </a>
+  </section>
+</template>
