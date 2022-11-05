50dd71684234c01502f7be88e1d516bb57b5f5a4
Ernie Jeash Villahermosa <40014179+KarmaBlackshaw@users.noreply.github.com>
2022-11-05 20:28:07 +0800

    wip: update gome view

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,3 +1,61 @@
+<script setup>
+// composables
+import useCase from '@/composables/useCase'
+
+// libs
+import { useAxios } from '@vueuse/integrations/useAxios'
+
+/**
+ * Cases
+ */
+const userInput = ref('')
+
+const {
+  cases
+} = useCase()
+
+const route = useRoute()
+
+const routeCaseType = computed(() => {
+  return route.params.case_type
+})
+
+watch(
+  () => route.params.case_type,
+  () => userInput.value = ''
+)
+
+const caseDetails = computed(() => {
+  return cases.find(item => item.slug === routeCaseType.value)
+})
+
+const output = computed(() => {
+  return caseDetails.value.handler(userInput.value)
+})
+
+/**
+ * Copy
+ */
+const {
+  copy,
+  copied
+} = useClipboard({ source: output })
+
+/**
+ * Sample text
+ */
+const {
+  data: randomQuoteData,
+  isLoading: isRandomQuoteLoading
+} = useAxios('https://api.quotable.io/random')
+
+const sampleData = computed(() => {
+  const caseItem = cases.find(item => item.slug === routeCaseType.value)
+
+  return caseItem.handler(randomQuoteData.value.content)
+})
+</script>
+
 <template>
   <section class="home">
     <div class="home__header header">
@@ -20,7 +78,10 @@
           <p class="mb-5">
             {{ randomQuoteData.content }}
           </p>
-          <p>=> {{ caseDetails.handler(randomQuoteData.content) }}</p>
+          <p>
+            <icon-material-symbols:keyboard-double-arrow-right class="inline" />
+            {{ sampleData }}
+          </p>
         </div>
       </div>
     </div>
@@ -53,101 +114,146 @@
             placeholder="Enter text to see output"
           ></textarea>
 
-          <base-button
-            class="editor__btn editor__btn--copy"
+          <button
+            class="
+              absolute
+              bottom-[10px]
+              right-[10px]
+              bg-emerald-500
+              hover:bg-emerald-600
+              text-white
+              transition
+              rounded-lg
+              px-5 py-3
+            "
             @click="copy()"
           >
-            {{ copied ? 'Copied' : 'Copy' }}
-          </base-button>
+            <div class="flex items-center gap-2">
+              <icon-material-symbols:content-copy-outline />
+
+              {{ copied ? 'Copied' : 'Copy' }}
+            </div>
+          </button>
         </div>
       </div>
     </div>
   </section>
 </template>
 
-<script>
-// libs
-import { useClipboard } from '@vueuse/core'
-import { ref, computed, watch } from 'vue'
-import { useRoute } from 'vue-router'
+<style lang="scss" scoped>
+.home {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-left: 10px;
 
-// composables
-import useCase from '@/composables/useCase'
+  $border-color-light: theme("colors.gray.200");
+  $border-color-dark: theme("colors.gray.700");
 
-// libs
-import { useAxios } from '@vueuse/integrations/useAxios'
+  .home__header.header {
+    width: 100%;
+    margin-top: 10px;
+    margin-bottom: 30px;
 
-export default {
-  name: 'HomeView',
-
-  setup () {
-
-    /**
-     * Cases
-     */
-    const userInput = ref('')
-
-    const {
-      cases
-    } = useCase()
-
-    const route = useRoute()
-
-    const routeCaseType = computed(() => {
-      return route.params.case_type
-    })
-
-    watch(
-      () => route.params.case_type,
-      () => userInput.value = ''
-    )
-
-    const caseDetails = computed(() => {
-      return cases.find(item => item.slug === routeCaseType.value)
-    })
-
-    const output = computed(() => {
-      return caseDetails.value.handler(userInput.value)
-    })
-
-    function clear () {
-      userInput.value = ''
+    .header__title {
+      font-size: 1.1em;
+      font-weight: 600;
+      font-family: Quicksand;
+      position: relative;
     }
 
-    /**
-     * Copy
-     */
-    const {
-      copy,
-      copied
-    } = useClipboard({ source: output })
+    .header__subtitle {
+      font-size: 0.9em;
+      letter-spacing: 0.025em;
+      margin-bottom: 10px;
+    }
 
-    /**
-     * Sample text
-     */
-    const {
-      data: randomQuoteData,
-      isLoading: isRandomQuoteLoading
-    } = useAxios('https://api.quotable.io/random')
+    .header__sample {
+      background-color: theme("colors.gray.700");
+      padding: 10px;
+      border-radius: 5px;
+      color: theme("colors.gray.100");
+      white-space: pre;
+      overflow: auto;
+    }
+  }
 
-    return {
-      caseDetails,
+  .editor {
+    width: 100%;
+    // height: 100%;
+    flex: 1 1 auto;
+    border: 1px solid $border-color-light;
+    display: flex;
+    margin-bottom: 10px;
 
-      clear,
-      copy,
-      copied,
+    @include dark {
+      border: 1px solid $border-color-dark;
+    }
+  }
 
-      output,
+  .editor__item {
+    flex: 1 1 auto;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
 
-      userInput,
+    &.editor__item--left {
+      border-right: 1px solid $border-color-light;
 
-      randomQuoteData,
-      isRandomQuoteLoading
+      @include dark {
+        border-right: 1px solid $border-color-dark;
+      }
+    }
+  }
+
+  .editor__title {
+    padding: 5px 10px;
+    border-bottom: 1px solid $border-color-light;
+    font-family: "Fira Mono", monospace;
+    text-transform: uppercase;
+    font-size: 0.8em;
+
+    @include dark {
+      border-bottom: 1px solid $border-color-dark;
+    }
+  }
+
+  .editor__body {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 5px;
+    position: relative;
+
+    textarea {
+      height: inherit;
+      width: 100%;
+      margin: 0;
+      border: none;
+      padding: 10px;
+      background-color: transparent;
+      resize: none;
+
+      &:focus {
+        border: none;
+        outline: none;
+      }
+    }
+  }
+
+  .editor__btn {
+    border-radius: 20px;
+
+    &.editor__btn--copy {
+      position: absolute;
+      bottom: 10px;
+      right: 10px;
+      width: 100px;
+      transition: background-color 200ms ease;
+      color: theme("colors.neutral.700");
+      background: #42d392;
     }
   }
 }
-</script>
 
-<style lang="scss" scoped>
-@import './assets/scss/HomeView';
 </style>
